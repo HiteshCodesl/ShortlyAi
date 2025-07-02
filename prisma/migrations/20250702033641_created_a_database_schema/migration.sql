3ff728fdddd507e207aa2fadf9eb1aad1a792225
Hitesh Navare <navarehitesh65@gmail.com>
2025-07-02 11:30:17 +0530

    added the schema and prisma client

---
+++ prisma/migrations/20250702033641_created_a_database_schema/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "subscription" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_id_key" ON "user"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
