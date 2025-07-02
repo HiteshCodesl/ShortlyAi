50d1a45fc2a0513c9b6b91d17d598cf7e0b08917
Hitesh Navare <navarehitesh65@gmail.com>
2025-07-02 11:29:40 +0530

    added providers

---
+++ app/providers.tsx
@@ -0,0 +1,13 @@
+
+import { SessionProvider } from "next-auth/react"
+import React from "react"
+
+function providers({children}: {children: React.ReactNode}) {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
+}
+
+export default providers
