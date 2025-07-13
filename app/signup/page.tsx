a210ceeeb9f256510e39cd203a43ea79007f3c89
Hitesh Navare <navarehitesh65@gmail.com>
2025-07-13 16:14:31 +0530

    added the pretty signin page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,56 +1,104 @@
-"use client"
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-dropdown-menu";
 import axios from "axios";
 import { signIn } from "next-auth/react";
-import { useState } from "react"
+import { useState } from "react";
 
 export default function SignUpPage() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState("");
-    const [name, setName] = useState("");
-    const [password, setPassword] = useState("");
- 
-    const signup = async() =>{
-        const user = {
-            email, password, name
-        }
-    try{
-      await axios.post("/api/auth/register", user, {
-        headers: {
-            "Content-Type": "application/json"
-        }
-      })
-        await signIn("credentials",{
-            name,
-            email,
-            password,
-            callbackUrl: "/"
-        })
-      } catch (err){
-        console.error("signup error:", err)
-      }
+  const signup = async () => {
+    try {
+      await axios.post("/api/auth/register", {
+        email,
+        name,
+        password,
+      });
+
+      await signIn("credentials", {
+        email,
+        password,
+        callbackUrl: "/",
+      });
+    } catch (err) {
+      console.error("Signup error:", err);
     }
-    
+  };
+
   return (
-    <div >
-      <input 
-      type="email" 
-      value={email} 
-      onChange={(e)=> setEmail(e.target.value)} 
-      placeholder="Enter a email" />
-
-      <input 
-      type="text" 
-      value={name} 
-      onChange={(e)=> setName(e.target.value)} 
-      placeholder="Enter a name"/>
-
-      <input 
-      type="password" 
-      value={password} 
-      onChange={(e)=> setPassword(e.target.value)} 
-      placeholder="Enter a password"/>
-
-      <button onClick={signup}>Signup</button>
-    </div>
-  )
+        <div className="flex justify-center h-screen my-auto items-center">
+          <Card className="bg-white">
+          <CardHeader>
+            <CardTitle className="text-2xl">Signup</CardTitle>
+            <CardDescription className="font-semibold">
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                  onChange={(e)=>{
+                    setEmail(e.target.value)
+                  }}
+                  className="font-semibold"
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="name">Name</Label>
+                  <Input
+                  onChange={(e)=>{
+                    setName(e.target.value)
+                  }}
+                  className="font-semibold"
+                    id="name"
+                    type="text"
+                    placeholder="Enter your name"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="#"
+                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline font-semibold"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input onChange={(e)=>{
+                    setPassword(e.target.value)
+                  }} className="font-semibold" id="password" type="password" placeholder="Enter a password" required />
+                </div>
+                <Button onClick={signup} type="submit" className="w-full">
+                  Login
+                </Button>
+                <Button variant="secondary" className="w-full">
+                  Login with Google
+                </Button>
+              </div>
+              <div className="mt-4 text-center text-sm font-semibold">
+                Don&apos;t have an account?{" "}
+                <a href="/login" className="underline underline-offset-4">
+                  Sign In
+                </a>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+  );
 }
