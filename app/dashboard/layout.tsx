26c274c87c1165405467653b84076c229be32141
Hitesh Navare <navarehitesh65@gmail.com>
2025-07-13 16:19:42 +0530

    added all the providers

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,25 +1,40 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Header } from "./_components/Header";
-import { SideNav } from "./_components/SideNav";
+import { AppSidebar } from "@/components/ui/app-sidebar";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { VideoDataContext } from "../_context/VideoDataContext";
+import ProgressBar from "../components/ProgressBar";
 
-export default function DashboardLayout({children}: {children: React.ReactNode}) {
-
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [videoData, setVideoData] = useState([]); 
   return (
-    <ThemeProvider  
-              attribute="class"
-              defaultTheme="dark"
-              enableSystem>
-   <div className={`dashboard-theme h-screen w-screen flex bg-background text-foreground font-sans`}>
-    <div>
-      <SideNav />
-    </div>
-    <div>
-       <Header />
-      {children}
-    </div>
-   </div> 
-  </ThemeProvider>
-  )
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}>
+        <VideoDataContext.Provider value={{ videoData, setVideoData }}>
+          <SidebarProvider>
+            <div className="dashboard-theme min-h-screen w-full flex flex-col">
+              
+              <Header />
+              <div className="flex flex-1">
+                <AppSidebar />
+
+                <main className="flex-1">
+                  <SidebarTrigger className="border btn-outline m-2 flex" />
+                  <ProgressBar />
+                  {children}
+                </main>
+              </div>
+            </div>
+          </SidebarProvider>
+        </VideoDataContext.Provider>
+    </ThemeProvider>
+  );
 }
