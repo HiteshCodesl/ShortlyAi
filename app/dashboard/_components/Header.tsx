d27de1df3771e06bd5f2df43514df3a078bf6a86
Hitesh Navare <navarehitesh65@gmail.com>
2025-07-13 16:18:54 +0530

    added the headers for dashboard

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -1,10 +1,12 @@
 import { ModeToggle } from '@/components/ui/toggle'
 import { Sparkles, Video } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
+import { TokenHeader } from './Token'
 
 export function Header() {
   return (
-    <div className='sticky flex items-center mt-2   gap-28 justify-around w-screen border-b pb-2'>
+    <div className='sticky flex items-center mt-2 gap-18 justify-around w-screen border-b pb-3'>
          <div className="relative flex items-center gap-2">
               <div className="w-10 h-10 bg-gradient-hero rounded-lg flex items-center justify-center glow-primary">
                 <Video className="h-6 w-6 text-white " />
@@ -15,13 +17,19 @@ export function Header() {
               </div>
          </div>
              <div className="flex md:flex items-center space-x-4 justify-end">
+           <TokenHeader />
            <ModeToggle />
-            <button className="hidden md:flex text-foreground transition-colors px-4 py-2 rounded-lg border ">
+         
+           <Link href={"/signup"}>
+            <button className="hidden md:flex text-foreground transition-colors px-4 py-2 rounded-lg border hover:bg-primary">
               Sign In
             </button>
+            </Link>
+            <Link href={"/dashboard"}>
             <button className="hidden md:flex btn-primary items-center mb-2">
              Dashboard
             </button>
+            </Link>
           </div>
     </div>
   )
